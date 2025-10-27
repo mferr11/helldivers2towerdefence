87836f8246825bec92e3acd354ebbbaaf9cb6aa0
mferr11 <mitchellferreira13@gmail.com>
2025-10-27 22:58:45 +1000

    Created wave class

---
+++ source/core/src/main/com/csse3200/game/waveSystem/wave.java
@@ -0,0 +1,45 @@
+package com.csse3200.game.waveSystem;
+
+import com.csse3200.game.entities.Entity;
+
+import java.util.List;
+
+/**
+ * Represents a wave of enemies with configurable composition and multiple possible paths.
+ */
+public class wave {
+    private final int waveNumber;
+    private final boolean isBossWave;
+    private final int numberOfEnemies;
+    private final float spawnRate; // Enemies per second
+    private final List<Entity> waypoints;
+
+    public wave(int waveNumber, boolean isBossWave, int numberOfEnemies, float spawnRate, List<Entity> waypoints) {
+        this.waveNumber = waveNumber;
+        this.isBossWave = isBossWave;
+        this.numberOfEnemies = numberOfEnemies;
+        this.spawnRate = spawnRate;
+        this.waypoints = waypoints;
+    }
+
+    /**
+     * Returns whether this wave is a boss wave.
+     */
+    public boolean isBossWave() {
+        return isBossWave;
+    }
+
+    /**
+     * Gets the total number of enemies in this wave.
+     */
+    public int getTotalEnemies() {
+        return numberOfEnemies;
+    }
+
+    /**
+     * Gets the current wave number.
+     */
+    public int getWaveNumber() { 
+        return waveNumber;
+    }
+}
